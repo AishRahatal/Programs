c59a774c864399943d0b0db3bef6829a1b4055cf
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-19 13:14:24 +0530

    generating distinct coupon numbers

--- LogicalProgram/CouponNumber.cs
+++ LogicalProgram/CouponNumber.cs
@@ -3,125 +3,81 @@ using System.Collections.Generic;
 
 namespace Coupon
 {
-    class CouponNumber
-    {
-        static void Main(string[] args)
-        {
+	class CouponNumber
+	{
+		static void Main(string[] args)
+		{
 
 
-            Console.WriteLine(" Program to generate a distinct coupon number");
-            Console.WriteLine("--------------------------------------------------------------------");
+			Console.WriteLine(" Program to generate a distinct coupon number");
+			Console.WriteLine("--------------------------------------------------------------------");
 
-            Console.WriteLine("How many random numbers do you need to generate coupon number?:  ");
-            int n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("How many random numbers do you need to generate coupon number?:  ");
+			int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("---------------------------------------------------------------------");
-            //int get_coupon(int n);
-            
-            int c = get_coupon(n);
+			Console.WriteLine("---------------------------------------------------------------------");
+			//int get_coupon(int n);
+			
+			
 
-            if (n > 0 && n <= 9)
-            {
+			if (num >0)
+			{
+				int c = get_coupon(num);
 
-                Console.WriteLine(" A distinct coupon number: " + c);
+			}
+			else
+			{
+				Console.WriteLine("Please enter valid number") ;
 
-                Console.WriteLine("------------------------------------------------------");
-
-
-            }
-            else
-            {
-                Console.WriteLine("Please enter number between 0-9") ;
-
-            }
+			}
 
 
-            int get_coupon(int num)
-            {
+         	int get_coupon(int num) 
+                 {
 
-                Random random = new Random();
-                int coupon;
+			Random random = new Random();
+			int[] coupon=new int[num];
+		
+					  
+					for (int i = 0; i < num; i++)
+					{
+						coupon[i] = random.Next(100000000); // generating coupon number
+					}
+					for (int i = 0; i < coupon.Length; i++)
+					{
+						Console.WriteLine("Random number " +(i+1) +" generated :" + coupon[i]); 
+					}
+						Console.WriteLine("------------------------------------------------------");
+				        
+				         
+		           Console.WriteLine("Distinct coupon number:");
 
-                switch (num)
-                {
+					for (int i = 0; i < num; i++) // checking coupon number is repeated 
+					{
+						bool is_repeated= false;
+						for (int j = 0; j < i; j++)
+						{
+							if (coupon[i] == coupon[j])
+							{
+							  is_repeated = true;
+								break;
+							}
+						}
 
-                    case 1:
+						if (!is_repeated)
+						{
+							Console.Write("{0} ",coupon[i]); // printing destict numbers
 
-                        coupon = random.Next(10);
-                        Console.WriteLine("Total random number reuired is 1");
-                        Console.WriteLine("------------------------------------------------------");
+						}
+					}
 
-                        return coupon;
-                        break;
-                    case 2:
+					Console.WriteLine("\n------------------------------------------------------");
+                             return 0;
+		      }
 
-                        coupon = random.Next(100);
-                        Console.WriteLine("Total random number reuired is 2");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 3:
-
-                        coupon = random.Next(1000);
-                        Console.WriteLine("Total random number reuired is 3");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 4:
-
-                        coupon = random.Next(10000);
-                        Console.WriteLine("Total random number reuired is 4");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 5:
-
-                        coupon = random.Next(100000);
-                        Console.WriteLine("Total random number reuired is 5");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 6:
-                        coupon = random.Next(1000000);
-                        Console.WriteLine("Total random number reuired is 6");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 7:
-                        coupon = random.Next(10000000);
-                        Console.WriteLine("Total random number reuired is 7");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 8:
-                        coupon = random.Next(100000000);
-                        Console.WriteLine("Total random number reuired is 8");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
-                    case 9:
-                        coupon = random.Next(1000000000);
-                        Console.WriteLine("Total random number reuired is 9");
-                        Console.WriteLine("------------------------------------------------------");
-
-                        return coupon;
-                        break;
+			Console.ReadKey();
 
 
-                }
-                return 0;
-            }
-
-
-
-
-        }
-    }
+		}
+	}
 }
